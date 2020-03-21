6619d28a84530bec953aba09bf65fb2a5869acd3
neu5 <neus90@gmail.com>
2020-03-21 19:04:45 +0100

    add .gitignore

--- index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const PORT = process.env.PORT || 5000
-
-http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end('nodejs-email');
-}).listen(PORT);
